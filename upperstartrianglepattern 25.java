25c14a015d306f65b50a10997f7c9858d9a39edb
subhankar347 <palsubhankar116@gmail.com>
2025-02-19 10:46:24 +0530

    Add files via upload

---
+++ upperstartrianglepattern 25.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class UpperStarTrianglePattern {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+    
+        System.out.print("Enter the number of rows: ");
+        int rows = scanner.nextInt();  
+
+        
+        for (int i = 1; i <= rows; i++) {
+            
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");  
+            }
+
+            
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");  
+            }
+
+        
+            System.out.println();
+        }
+
+
+        scanner.close();
+    }
+}
