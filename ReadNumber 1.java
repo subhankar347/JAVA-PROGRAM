d3caed02f77f6e8f8baed225a380b95a13c27086
subhankar347 <palsubhankar116@gmail.com>
2025-02-19 10:12:27 +0530

    Add files via upload

---
+++ ReadNumber 1.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class ReadNumber {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+
+        int number = scanner.nextInt();
+
+        System.out.println("You entered: " + number);
+
+    
+        scanner.close();
+    }
+}
+
