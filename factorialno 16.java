db9e5c571291dccdf35cc85ed6479a2d492e67fe
subhankar347 <palsubhankar116@gmail.com>
2025-02-19 10:21:32 +0530

    Add files via upload

---
+++ factorialno 16.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();  
+        long factorial = calculateFactorial(number);
+
+        System.out.println("The factorial of " + number + " is: " + factorial);
+
+        scanner.close();
+    }
+
+    public static long calculateFactorial(int num) {
+        long result = 1;
+
+        for (int i = 1; i <= num; i++) {
+            result *= i;
+        }
+
+        return result;
+    }
+}
